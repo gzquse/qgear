c098eb0a0c1c008b2f23c9b3f166f9a8bc584ff1
gzquse <ziqinguse@gmail.com>
2024-07-03 12:03:27 -0700

    multiple gpus update

--- benchmark/cuquantum_backends.py
+++ benchmark/cuquantum_backends.py
@@ -4,22 +4,34 @@
 #
 # This will take a noticeably longer time to execute on
 # CPU-only backends.
-
+# mpirun -np 4 --allow-run-as-root python3 cuquantum_backends.py
 import cudaq
-
+from time import time
 # qubit_count = 5
 # We can set a larger `qubit_count` if running on a GPU backend.
 qubit_count = 34
 cudaq.set_target("nvidia-mgpu")
+ranks=-1; myrank=0
+'''
+cudaq.mpi.initialize()
+ranks = cudaq.mpi.num_ranks()
+myrank = cudaq.mpi.rank()
+'''
+
 
 @cudaq.kernel
 def kernel(qubit_count: int):
     qvector = cudaq.qvector(qubit_count)
     h(qvector)
-    for qubit in range(qubit_count - 1):
-        x.ctrl(qvector[qubit], qvector[qubit + 1])
+    for j in range(50):
+        for qubit in range(qubit_count - 1):
+            x.ctrl(qvector[qubit], qvector[qubit + 1])
     mz(qvector)
 
-
-result = cudaq.sample(kernel, qubit_count, shots_count=2000000)
-print(result)
+T0=time()
+result=cudaq.sample(kernel, qubit_count, shots_count=10)
+elaT=time()-T0
+if myrank==0:
+    print('elaT=%.1fsec , qubit_count=%d, num output=%d myrank=%d \
+      of %d'%(elaT,qubit_count,len(result),myrank,ranks))
+    #print(myrank,result)
