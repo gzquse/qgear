87f607aa3c7847e362607c7e27b1e62f81e46717
gzquse <ziqinguse@gmail.com>
2024-07-10 12:03:14 -0700

    minimum try

--- explore/strawman/simple_ghz50_cudaq.py
+++ explore/strawman/simple_ghz50_cudaq.py
@@ -13,20 +13,9 @@ from time import time
 # We can set a larger `qubit_count` if running on a GPU backend.
 qubit_count = 35
 target="nvidia-mgpu"
-cudaq.mpi.initialize()
 
 #cudaq.set_target("nvidia-mqpu")  # parallel-GPU
 cudaq.set_target(target)  # adj-GPU
-ranks = cudaq.mpi.num_ranks()
-myrank = cudaq.mpi.rank()
-
-if myrank == 0:
-    print('\n start nq=%d, target=%s  ranks=%d... '%(qubit_count,target,ranks))
-    
-if cudaq.num_available_gpus() == 0:
-    print("This example requires a GPU to run. No GPU detected.")
-    exit(0)
-
 
 @cudaq.kernel
 def kernel(qubit_count: int):
@@ -34,8 +23,6 @@ def kernel(qubit_count: int):
     h(qvector)
     for j in range(50):
         for qubit in range(qubit_count - 1):
-            #ry(0.1, qvector[qubit])
-            #rz(0.2, qvector[qubit+1])
             x.ctrl(qvector[qubit], qvector[qubit + 1])
     mz(qvector)
 
@@ -45,10 +32,6 @@ T0=time()
 result=cudaq.sample(kernel, qubit_count, shots_count=10)
 elaT=time()-T0
 
-# print("num of gpus:", cudaq.num_available_gpus())
-# print('elaT=%.1fsec , qubit_count=%d, num output=%d '%(elaT,qubit_count,len(result)))
-if myrank == 0:
-    #1print('counts:',result)
-    print('\nelaT=%.1fsec , nq=%d, target=%s, ranks=%d, num output=%d '%(elaT,qubit_count,target,ranks,len(result)))
+
+print('\nelaT=%.1fsec , nq=%d, target=%s, num output=%d '%(elaT,qubit_count,target,len(result)))
     
-cudaq.mpi.finalize()
