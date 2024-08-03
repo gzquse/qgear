5be6ca7da4ed6f4a8a88ae0cd80aa3d4afddbc75
Jan Balewski <janstar1122@gmail.com>
2024-08-02 20:36:54 -0700

    QCRank is ready

---
+++ benchmark_qcrank/prep_cannedImage.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+__author__ = "Jan Balewski"
+__email__ = "janstar1122@gmail.com"
+
+'''
+Create canned input image for QCrank
+
+'''
+import copy
+from pprint import pprint
+import numpy as np
+from PIL import Image
+import os,sys
+from toolbox.Util_H5io4 import  write4_data_hdf5, read4_data_hdf5
+#from toolbox.Util_EscherHands import ehandsInput_to_qcrankInput
+
+import argparse
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v","--verbosity",type=int,choices=[0, 1, 2],  help="increase output verbosity", default=1, dest='verb')
+    parser.add_argument("--basePath",default=None,help="head path for set of experiments, or 'env'")
+    parser.add_argument("--outPath",default='out/',help="output from ppacking image")
+
+    parser.add_argument("-t","--tag",  default='b2',help="hardcoded configuration defining type of the image ")
+    parser.add_argument('-q','--nqAddr', default=9, type=int, help='paralelizm of QCrank encoding ')
+
+    args = parser.parse_args()
+    # make arguments  more flexible
+    if args.basePath=='env': args.basePath= os.environ['Cudaq_dataVault']
+    if args.basePath!=None:
+        args.outPath=os.path.join(args.basePath,'jobs')
+    
+    args.inpPath='../input_gray_images'
+ 
+    print( 'myArg-program:',parser.prog)
+    for arg in vars(args):  print( 'myArg:',arg, getattr(args, arg))
+    assert os.path.exists(args.outPath)
+    return args
+
+#...!...!....................
+def importImage(args):
+    tag=args.tag
+    inpN=None
+    if tag=='a1':  inpN='alphabet_x32_y1'
+    if tag=='b1':  inpN='alphabet_x64_y16' 
+    if tag=='b2':  inpN='high-heels_x32_y32' 
+    if tag=='c1':  inpN='xray-finger-8y_x64_y80' 
+    if tag=='d1':  inpN='igb_facade_x192_y128' 
+    if tag=='d2':  inpN='zebra_x192_y128' 
+    if tag=='d3':  inpN='xray-hand-4y_x128_y192' 
+    if tag=='d4':  inpN='xray-foot-12y_x192_y128' 
+    if tag=='d5':  inpN='e_coli_bacteria_x192_y128' 
+    if tag=='e1':  inpN='zebra_x384_y256' 
+    if tag=='e2':  inpN='cameraman_x256_y384' 
+    if tag=='e3':  inpN='micro_germ_x384_y256' 
+    if tag=='x':  inpN=1 ;  nqAddr=1
+    if tag=='x':  inpN=1 ;  nqAddr=1
+
+    if inpN==None: print('undefined tag, abort'); exit(99)
+    inpFF=os.path.join(args.inpPath,inpN+'.png')
+    img = Image.open(inpFF)
+    imgA=np.array(img)
+    print('imported imgA:',inpN,imgA.shape,imgA.dtype)
+    args.image_name=inpN
+    bigD={'phys_image':imgA}
+
+    
+    return bigD
+
+
+#...!...!....................
+def buildMeta_CannedImage(args,bigD):
+    pd={}  # payload
+    pd['num_sample']=1
+    pixY,pixX=bigD['phys_image'].shape
+    
+    cad={} # canned MD
+    cad['image_name']=args.image_name
+    cad['image_shape_xy']=[pixX,pixY]
+    cad['image_pixels']=pixX*pixY
+        
+    pd['nq_addr']=args.nqAddr
+    pd['seq_len']=1<<pd['nq_addr']
+    assert cad['image_pixels']% pd['seq_len']==0
+    pd['nq_fdata']= cad['image_pixels']// pd['seq_len']
+    pd['num_clbit']=pd['nq_fdata']+pd['nq_addr']  # number of measured qubits
+    assert pd['num_clbit'] <34  # needs GPU's to run, Martin change it to a higher value
+    
+    pd['qcrank_max_fval']=np.pi # default range for QCrank
+    
+    cad['canned_type']='gray_image'
+    md={ 'payload':pd,'canned':cad}
+    
+    md['short_name']='canImg_%s_%d_%d'%(args.tag,pixX,pixY)
+
+    print('input phys image:',cad)
+    if args.verb>1:  print('\nBMD:');pprint(md)            
+    return md
+
+#...!...!....................
+def ehandsInput_to_qcrankInput(udata):  # shape: n_img,  nq_data, n_addr,
+    fdata=np.arccos(udata) # encode user data for qcrank
+    # QCrank wants indexs order  to be: n_addr, nq_data, n_img --> (2,1,0)
+    fdata=np.transpose(fdata, ( 2,1, 0))
+    # QCrank also reverses the order of data qubits
+    # Reverse the order of values along axis 1
+    fdata = fdata[:, ::-1, :]
+    return fdata  # shape : n_addr, nq_data, n_img
+
+#...!...!....................
+def prepImageQCrankInput(md,bigD):
+    pmd=md['payload']
+    cad=md['canned']
+    
+    n_addr=pmd['seq_len']
+    nq_data=pmd['nq_fdata']
+    n_img=pmd['num_sample']
+    #imgOp=pmd['math_op']
+    #nEHbl=pmd['ehands_blocks']
+    
+    #.... Renormalize the array  values to range [-1,1] needed by Escherhands
+    imgA=bigD['phys_image']
+    min_val = imgA.min()
+    max_val = imgA.max()
+    imgAN = 2 * ((imgA - min_val) / (max_val - min_val)) - 1
+    #print('imgAN:',imgAN.dtype,imgAN.shape,imgAN[:])
+    print('img min/max:',imgAN.min(),imgAN.max())    
+    bigD['norm_image']=imgAN.astype(np.float32)
+
+    # Flatten along x-axis (row-wise)
+    inpA=imgAN.flatten()        # Default is C-style order
+
+    print('PQCT: inpA:',inpA.shape, inpA.reshape(-1,n_addr).shape)
+    
+    #.... final format for QCrank
+    inp_udata = np.zeros((n_img, nq_data, n_addr),dtype=np.float32)
+    assert n_img==1; im=0
+    inp_udata[im] =inpA.reshape(-1,n_addr)
+    print('inp_udata:',inp_udata.shape)
+    
+    bigD['inp_udata']=inp_udata  # shape: n_img, nq_data, n_addr
+    bigD['inp_fdata']=ehandsInput_to_qcrankInput(inp_udata)  # shape : n_addr, nq_data, n_img
+    
+
+    
+#=================================
+#=================================
+#  M A I N 
+#=================================
+#=================================
+if __name__=="__main__":
+    args=get_parser()
+    bigD=importImage(args)   
+    MD=buildMeta_CannedImage(args,bigD)
+    pprint(MD)
+    
+    prepImageQCrankInput(MD,bigD)
+    
+      
+    print('M:inp bigD:',sorted(bigD))
+    
+    outF=MD['short_name']+'.qcrank_inp.h5'
+    fullN=os.path.join(args.outPath,outF)
+    write4_data_hdf5(bigD,fullN,metaD=MD)
+
+    pprint(MD)
+
+    print('local sim:\n time  ./run_aer_job.py --cannedExp   %s   -n 300   -E \n'%(MD['short_name'] ))
+    
+    print('M:done')
+   
---
+++ benchmark_qcrank/run_aer_job.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+__author__ = "Jan Balewski"
+__email__ = "janstar1122@gmail.com"
+
+'''
+QCrank
+
+Use Qiskit ideal  simulator
+use all-to-all connectivity.
+Run simulations w/o submit/retrieve of the job
+Simulations run locally.
+Records meta-data containing  job_id
+HD5 arrays contain input images
+Dependence: qpixl, qiskit
+'''
+
+import time
+import sys,os
+import numpy as np
+from pprint import pprint
+
+from qiskit_aer import AerSimulator
+from qiskit.visualization import circuit_drawer
+from qiskit import  transpile
+from toolbox.Util_ibm import harvest_ibmq_backRun_submitMeta, harvest_backRun_results, harvest_circ_transpMeta
+from toolbox.Util_H5io4 import  write4_data_hdf5, read4_data_hdf5
+from toolbox.Util_Qiskit import  circ_depth_aziz
+
+
+sys.path.append(os.path.abspath("/daan_qcrank/py"))
+from qpixl import qcrank
+import argparse
+
+#...!...!..................
+def get_parser(backName="ibmq_qasm_simulator",provName="local sim",doMathOp=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v","--verb",type=int, help="increase debug verbosity", default=1)
+    parser.add_argument("--basePath",default=None,help="head path for set of experiments, or 'env'")
+    parser.add_argument("--inpPath",default='out/',help="input packed image")
+    parser.add_argument("--outPath",default='out/',help="raw outputs from experiment")
+    parser.add_argument("--cannedExp",  default='canImg_b2_32_32',help='packed image name')
+    parser.add_argument("--expName",  default=None,help='(optional) replaces IBMQ jobID assigned during submission by users choice')
+    
+    # .... job running
+    parser.add_argument('-n','--numShotPerAddr',type=int,default=4000, help="shots per address of QCrank, or as-is if negative")
+    parser.add_argument( "-E","--executeCircuit", action='store_true', default=False, help="may take long time, test before use ")
+
+    args = parser.parse_args()
+    args.backend='aer'
+    # make arguments  more flexible
+    if args.basePath=='env': args.basePath= os.environ['Cudaq_dataVault']
+    if args.basePath!=None:
+        args.outPath=os.path.join(args.basePath,'jobs')
+    
+        
+    print( 'myArg-program:',parser.prog)
+    for arg in vars(args):  print( 'myArg:',arg, getattr(args, arg))
+    assert os.path.exists(args.outPath)
+    return args
+
+
+#...!...!..................
+def canned_qcrank_inp(args):
+    inpF=args.cannedExp+'.qcrank_inp.h5'
+    bigD,md=read4_data_hdf5(os.path.join(args.outPath,inpF))
+
+    sd={}
+    sd['num_shots']=args.numShotPerAddr*md['payload']['seq_len']
+    md['submit']=sd
+
+    return bigD,md
+
+#...!...!....................
+def qcrank_and_EscherHands(md, barrier=True):
+    # can store mutiple Ehands blocks in parallel
+    pmd=md['payload']
+    nq_addr=pmd['nq_addr']
+    nq_data=pmd['nq_fdata'] 
+    
+    #.... create parameterized QCrank  circ
+    qcrankObj = qcrank.ParametrizedQCRANK(
+        nq_addr, nq_data,
+        qcrank.QKAtan2DecoderQCRANK,
+            keep_last_cx=True, barrier=barrier,
+        measure=True, statevec=False,
+        reverse_bits=True  # to match Qiskit littleEndian
+    )
+    return  qcrankObj
+
+#=================================
+#=================================
+if __name__ == "__main__":
+    args=get_parser(backName='aer')
+    
+    expD,expMD=canned_qcrank_inp(args)
+        
+    pprint(expMD)
+    numShots=expMD['submit']['num_shots']
+    
+    #... adjust parser settings
+    #1args.outPath=os.path.join(args.basePath,'meas')    
+    #assert os.path.exists(args.outPath)
+    
+    # ------  circuit generation -------
+    qcrankObj=qcrank_and_EscherHands( expMD)
+    qcP=qcrankObj.circuit
+    nqTot=qcP.num_qubits
+    
+    print('M: circuit has %d qubits'%(qcP.num_qubits))
+    circ_depth_aziz(qcP,text='circ_orig')
+    
+    if args.verb>0 and qcP.num_qubits<7 or args.verb>1 :
+        print('M:.... PARAMETRIZED IDEAL CIRCUIT ..............')        
+        print(circuit_drawer(qcP, output='text',cregbundle=True))
+
+    backend = AerSimulator()
+   
+    qcT=qcP
+            
+    if  qcP.num_qubits<6:
+        print('M:.... PARAMETRIZED TRANSPILED CIRCUIT .............. for',backend)
+        print(qcT.draw(output='text',idle_wires=False))  # skip ancilla
+
+    harvest_circ_transpMeta(qcT,expMD,args.backend)
+     
+    # -------- bind the data to parametrized circuit  -------
+    f_data=expD['inp_fdata']
+    qcrankObj.bind_data(f_data, max_val=expMD['payload']['qcrank_max_fval'])
+    
+    # generate the instantiated circuits
+    qcEL = qcrankObj.instantiate_circuits()
+    nCirc=len(qcEL)
+    print('M: execution-ready %d circuits on %d qubits on %s'%(nCirc,nqTot,backend.name))                        
+    if not args.executeCircuit:
+        pprint(expMD)
+        print('NO execution of circuit, use -E to execute the job')
+        exit(0)
+
+    # ----- submission ----------
+    T0=time.time()
+    job =  backend.run(qcEL,shots=numShots)
+    jid=job.job_id()
+
+    print('submitted JID=',jid,backend ,'\n do %.2fM shots , wait for execution ...'%(numShots/1e6))
+    
+    harvest_ibmq_backRun_submitMeta(job,expMD,args)
+    T1=time.time()
+    print(' job done, elaT=%.1f min'%((T1-T0)/60.))
+    
+    print('M: got results')
+    harvest_backRun_results(job,expMD,expD)
+    
+    outF=os.path.join(args.outPath,expMD['short_name']+'.h5')
+    write4_data_hdf5(expD,outF,expMD)
+
+    print('   ./postproc_exp.py --expName   %s --showPlots b c   \n'%(expMD['short_name']))
+ 
+
+     
